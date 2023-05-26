fd96face74deae1ed5225773bbe1a8c1ed364d1b
Erlin Puspa Ningrum <116940126+erlinlin@users.noreply.github.com>
2023-05-26 08:07:37 +0700

    Login and Registration API source code
    notes:
    SQL code to create User table
    -----------------------------------------------------------------------------------------
    CREATE TABLE User (
      id INT AUTO_INCREMENT PRIMARY KEY,
      username VARCHAR(50) NOT NULL,
      password VARCHAR(255) NOT NULL,
      email VARCHAR(100) NOT NULL,
      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
    );

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, request, jsonify
+import mysql.connector
+
+app = Flask(__name__)
+
+# Konfigurasi database
+db = mysql.connector.connect(
+    host="localhost", # Ganti sesuai dengan pengaturan database phpMyAdmin
+    user="username", # Ganti sesuai dengan pengaturan database phpMyAdmin 
+    password="password", # Ganti sesuai dengan pengaturan database phpMyAdmin
+    database="database_name" # Ganti sesuai dengan pengaturan database phpMyAdmin
+)
+
+# Route untuk login
+@app.route('/login', methods=['POST'])
+def login():
+    username = request.json['username']
+    password = request.json['password']
+
+    cursor = db.cursor()
+
+    # Query untuk memeriksa keberadaan username dan password yang cocok di database
+    query = "SELECT * FROM users WHERE username = %s AND password = %s"
+    values = (username, password)
+    cursor.execute(query, values)
+
+    # Memeriksa hasil query
+    if cursor.rowcount == 1:
+        # Jika login berhasil, kirimkan pesan berhasil
+        return jsonify({'message': 'Login berhasil!'})
+    else:
+        # Jika login gagal, kirimkan pesan gagal
+        return jsonify({'message': 'Login gagal!'})
+
+# Route untuk registrasi
+@app.route('/register', methods=['POST'])
+def register():
+    username = request.json['username']
+    password = request.json['password']
+    email = request.json['email']
+
+    cursor = db.cursor()
+
+    # Query untuk memeriksa apakah username atau email sudah digunakan
+    check_query = "SELECT * FROM users WHERE username = %s OR email = %s"
+    check_values = (username, email)
+    cursor.execute(check_query, check_values)
+
+    # Memeriksa hasil query
+    if cursor.rowcount > 0:
+        # Jika username atau email sudah ada, kirimkan pesan gagal
+        return jsonify({'message': 'Username atau email sudah digunakan!'})
+
+    # Jika username atau email belum digunakan, lakukan registrasi
+    insert_query = "INSERT INTO users (username, password, email) VALUES (%s, %s, %s)"
+    insert_values = (username, password, email)
+    cursor.execute(insert_query, insert_values)
+    db.commit()
+
+    # Registrasi berhasil, kirimkan pesan berhasil
+    return jsonify({'message': 'Registrasi berhasil!'})
+
+if __name__ == '__main__':
+    app.run()
